623e59453d18031944f0db0ed91dae46ba4c6ce7
Cyril Olanolan <57930177+CyrilOlanolan@users.noreply.github.com>
2022-07-26 23:40:43 +0800

    Fixed hero section

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,28 +1,34 @@
-import React from 'react'
-import Image from 'next/image'
-import styles from './LandingPage.module.scss';
+import React from "react";
+import Image from "next/image";
+import styles from "./LandingPage.module.scss";
 
 const LandingPage = () => {
   return (
     <div className={styles["LandingPage"]}>
-      <div class={styles["grid-container"]}>
+      <div className={styles["grid-container"]}>
         <div className={`${styles["grid-item"]} ${styles["one"]}`}>
+          <div className={styles["image_wrapper"]}>
             <Image
-              src={"/addu-logo.png"}
-              alt={"AdDU Logo"}
-              height={'200px'}
-              width={'200px'}
-              // layout={'fill'}
-              // objectFit={'scale-down'}
+              src="/addu-logo.png"
+              alt="AdDU Logo"
+              layout="fill"
+              objectFit="contain"
               priority
             />
+          </div>
         </div>
-        <div className={`${styles["grid-item"]} ${styles["two"]}`}><h1>SIDLAKAN 22</h1></div>
-        <div className={`${styles["grid-item"]} ${styles["three"]}`}><h1>CURRENTLY</h1></div>  
-        <div className={`${styles["grid-item"]} ${styles["four"]}`}><h1>CHEER DONT JEER</h1></div>
+        <div className={`${styles["grid-item"]} ${styles["two"]}`}>
+          <h1>SIDLAKAN 22</h1>
+        </div>
+        <div className={`${styles["grid-item"]} ${styles["three"]}`}>
+          <h1>CURRENTLY</h1>
+        </div>
+        <div className={`${styles["grid-item"]} ${styles["four"]}`}>
+          <h1>CHEER DONT JEER</h1>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
