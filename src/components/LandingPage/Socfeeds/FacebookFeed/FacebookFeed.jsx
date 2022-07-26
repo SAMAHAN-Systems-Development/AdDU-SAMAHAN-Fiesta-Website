d94e26852a891798d6c37657f1178d8d6769ca94
tremor6916 <lpawaon@gmail.com>
2022-07-26 19:28:07 +0800

    update FacebookFeed - okay na ang appID

--- src/components/LandingPage/Socfeeds/FacebookFeed/FacebookFeed.jsx
+++ src/components/LandingPage/Socfeeds/FacebookFeed/FacebookFeed.jsx
@@ -4,9 +4,9 @@ import { FacebookProvider, Page } from 'react-facebook';
 export default class FacebookFeed extends Component {
     render() {
       return (
-        <FacebookProvider appId="123456789"> 
+        <FacebookProvider appId="777843193336100"> 
           <Page href="https://www.facebook.com/SAMAHANSysDev" tabs="timeline" />
         </FacebookProvider>    
       );
     }
-  }
+}
