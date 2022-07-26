f3dd1f120a9c704cf0e24ac6f159476f3d38bbd8
iyabcd <68364708+iyabcd@users.noreply.github.com>
2022-07-26 20:35:09 +0800

    added pics to hero grid, partial media tags for grid

---
+++ .history/src/components/LandingPage/LandingPage_20220726190853.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Image from 'next/image'
+import styles from './LandingPage.module.scss';
+
+const LandingPage = () => {
+  return (
+    <div className={styles["LandingPage"]}>
+      <div class={styles["grid-container"]}>
+        <div className={`${styles["grid-item"]} ${styles["one"]}`}>
+        <Image
+          src={"/addu-logo.png"}
+          alt={"AdDU Logo"}
+          layout='center'
+          priority
+        />
+          <h1>LOGO</h1>
+        </div>
+        <div className={`${styles["grid-item"]} ${styles["two"]}`}><h1>SIDLAKAN 22</h1></div>
+        <div className={`${styles["grid-item"]} ${styles["three"]}`}><h1>Vas Happenin'?</h1></div>  
+        <div className={`${styles["grid-item"]} ${styles["four"]}`}><h1>CHEER DONT JEER</h1></div>
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage
