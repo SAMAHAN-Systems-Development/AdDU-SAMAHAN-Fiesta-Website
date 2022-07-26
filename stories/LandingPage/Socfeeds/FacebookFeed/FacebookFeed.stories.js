6f6aaf6327f61627b6afa5fcd28eb840fc80383b
tremor6916 <lpawaon@gmail.com>
2022-07-26 19:28:17 +0800

    init FacebookFeed stories

---
+++ stories/LandingPage/Socfeeds/FacebookFeed/FacebookFeed.stories.js
@@ -0,0 +1,10 @@
+import {FacebookFeed} from '../../../../src/components/ComponentIndex';
+
+export default {
+    title: 'Landing Page/Socfeeds/FacebookFeed/FacebookFeed',
+    component: FacebookFeed,
+};
+
+export const Main = {
+    args: {}
+}
