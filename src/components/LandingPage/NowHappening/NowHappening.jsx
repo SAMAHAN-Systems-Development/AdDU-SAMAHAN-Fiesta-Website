495082279e787fffa4aabb0b4ed051371f306806
tremor6916 <lpawaon@gmail.com>
2022-07-27 17:44:17 +0800

    updated NowHappening - linked to NowHappeningContent

--- src/components/LandingPage/NowHappening/NowHappening.jsx
+++ src/components/LandingPage/NowHappening/NowHappening.jsx
@@ -2,16 +2,16 @@ import React,{useState, useEffect} from "react";
 
 import styles from "./NowHappening.module.scss";
 
-
+import {FilterContents} from "../../../utilities/datetime";
+import NowHappeningContent from "./NowHappeningContent/NowHappeningContent";
 
 const NowHappening=()=>{
     return(
-    <>
         <div>
             <h1>Now Happening</h1>
+                <NowHappeningContent/>
         </div>
-    </>
-)
+    )
 }
 
 export default NowHappening;
