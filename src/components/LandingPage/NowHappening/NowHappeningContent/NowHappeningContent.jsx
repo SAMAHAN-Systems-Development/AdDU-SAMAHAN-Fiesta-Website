134197e4e0cafc78a282784adeb8b887c6cc3e45
tremor6916 <lpawaon@gmail.com>
2022-07-27 18:16:23 +0800

    update NowHappeningContent

--- src/components/LandingPage/NowHappening/NowHappeningContent/NowHappeningContent.jsx
+++ src/components/LandingPage/NowHappening/NowHappeningContent/NowHappeningContent.jsx
@@ -3,17 +3,17 @@ import React from "react";
 import styles from "./NowHappeningContent.module.scss";
 import CHData from "../../../../sampleData/currentlyHappening.json";
 
-const NowHappeningContent=({dateTime,event,platform})=>{
+const NowHappeningContent=()=>{
     return(
         <div>
             {CHData.content.contents.map((item)=>{
                 return(
                     <React.Fragment key={item.ItemNum}>
-                        dateTime={item.DateTime}
+                        {item.DateTime}
                         <br></br>
-                        event={item.event}
+                        {item.event}
                         <br></br>
-                        platform={item.platform}
+                        {item.platform}
                         <br></br>
                     </React.Fragment>
                 );
