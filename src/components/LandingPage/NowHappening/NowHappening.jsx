d9a56bce70f3c67407b026cfbbb0ed5e67043ae9
tremor6916 <lpawaon@gmail.com>
2022-07-27 16:05:05 +0800

    update NowHappening - added empty stuff

---
+++ src/components/LandingPage/NowHappening/NowHappening.jsx
@@ -0,0 +1,17 @@
+import React,{useState, useEffect} from "react";
+
+import styles from "./NowHappening.module.scss";
+
+
+
+const NowHappening=()=>{
+    return(
+    <>
+        <div>
+            <h1>Now Happening</h1>
+        </div>
+    </>
+)
+}
+
+export default NowHappening;
