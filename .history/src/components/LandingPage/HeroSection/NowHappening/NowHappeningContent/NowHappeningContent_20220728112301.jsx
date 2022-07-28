52cd8ff6afb28cecd33d661526293bcb9436d1d9
iyabcd <68364708+iyabcd@users.noreply.github.com>
2022-07-28 12:26:44 +0800

    fixed now happening animation and layout

---
+++ .history/src/components/LandingPage/HeroSection/NowHappening/NowHappeningContent/NowHappeningContent_20220728112301.jsx
@@ -0,0 +1,16 @@
+import styles from "./NowHappeningContent.module.scss";
+
+const NowHappeningContent = ({ time, event, platform , description }) =>{
+    return(
+        <div className={styles['container']}>
+            <div className={styles["details"]}>
+                <h4>{time}</h4>
+                <h2>{event}</h2>
+                <h3>{platform}</h3>
+                <p>{description}</p>
+            </div>
+        </div>
+    )
+}
+
+export default NowHappeningContent;
