f9000ca1e91b56627dd036f86b90c82eccbc6137
iyabcd <68364708+iyabcd@users.noreply.github.com>
2022-07-28 14:17:58 +0800

    fixed hero grid two(added vid bg), fixed hero grids hover animations, added socfeeds to socialssection, responsive all except navbar(button size not adjusted) and socfeeds components

---
+++ .history/src/components/LandingPage/HeroSection/NowHappening/NowHappening_20220728130614.jsx
@@ -0,0 +1,47 @@
+import React,{useState, useEffect} from "react";
+
+import styles from "./NowHappening.module.scss";
+// import NowHappeningContent from "./NowHappeningContent/NowHappeningContent";
+import { CHData } from '../../../../sampleData/dataIndex'
+import { NowHappeningContent } from '../../../ComponentIndex'
+import { FilterContents } from "../../../../utilities/datetime";
+import vid from '../../../../../public/vid.mp4';
+
+
+const NowHappening=()=>{
+
+    const [CHContent, SetCHContent ] = useState(CHData.content.contents);
+
+    useEffect(() => {
+        SetCHContent(FilterContents(CHContent));
+        
+        const interval = setInterval(() => {
+          SetCHContent(FilterContents(CHContent));
+        }, 10000)
+    
+        return () => clearInterval(interval);
+      }, [])    
+
+    return(
+        <div className={styles['container']}>
+            <h1>CURRENTLY</h1>
+            <div className={styles['content']}>
+                <video src={require('../../../../../public/vid.mp4')} />
+                {CHContent.map((item) => {
+                    return (
+                        <React.Fragment key={item.DateTime}>
+                        <NowHappeningContent
+                            time={item.DateTime}
+                            event={item.event}
+                            platform={item.platform}
+                            description={item.desc}
+                        />
+                        </React.Fragment>
+                    );
+                    })}
+            </div>
+        </div>
+    )
+}
+
+export default NowHappening;
---
+++ .history/src/components/LandingPage/LandingPage_20220728124036.jsx
@@ -0,0 +1,20 @@
+import { HeroSection } from "../ComponentIndex";
+import { CalendarSection } from "../ComponentIndex";\
+import { SocialsSection } from "../ComponentIndex";
+import Footer from "../Footer/Footer";
+import styles from "./LandingPage.module.scss";
+import SocialsSection from "./SocialsSection/SocialsSection";
+
+const LandingPage = () => {
+  return (
+    <div className={styles["LandingPage"]}>
+      <HeroSection />
+      <CalendarSection />
+      <Socfeeds />
+      <SocialsSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default LandingPage;
