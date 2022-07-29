68aa34c501d3a629d86e6154fb448ad1c15947a0
Innhahn <jaytan3825@gmail.com>
2022-07-30 00:03:52 +0800

    Added SYSDEV LOGO'S FOR FOOTER

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,18 +8,29 @@ const Footer = () => {
         <div className={styles['footer-left']}>
         <p> Copyright © 2022 SAMAHAN</p>
         <p> Developed by <a href = "">SAMAHAN Systems Development </a></p>
+     
+
         </div>
 
         <div className={styles['footer-right']}>
           <h3> SIDLAKAN </h3>
-            <div className={styles['img-wrapper']}>
+          <div className={styles['img-wrapper']}>
+            <Image
+              src="/assets/Smiley_Logo.png" 
+              alt="Smiley Sysdev Logo"
+              layout='fill'
+              objectFit='contain'
+            />
+          </div>
+
+          <div className={styles['img-wrapper']}>
               <Image
-                src="/assets/SYSDEV-Logo.png" 
-                alt="SysDev Logo"
-                layout='fill'
-                objectFit='contain'
-              />
-            </div>
+              src="/assets/SYSDEV_Logo.png" 
+              alt="SysDev Logo"
+              layout='fill'
+              objectFit='contain'
+            />
+          </div>
         </div>
     </div>
   )
