91bb2ee577acbc1afcf928fbfdee39af07970488
iyabcd <68364708+iyabcd@users.noreply.github.com>
2022-07-29 18:05:12 +0800

    added carousel for grids 2 to 3 for responsive, now happening content not showing when minimized (to fix)

---
+++ .history/src/components/LandingPage/HeroSection/HeroSection_20220729165708.jsx
@@ -0,0 +1,77 @@
+import Image from "next/image";
+import styles from "./HeroSection.module.scss";
+import NowHappening from "./NowHappening/NowHappening";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
+const HeroSection = () => {
+  
+  return (
+    <div className={styles["section"]}>
+      <div className={styles["grid-container"]}>
+        <div className={`${styles["grid-item"]} ${styles["one"]}`}>
+          <div className={styles["img_wrapper"]}>
+            <Image
+              src="/addu-logo.png"
+              alt="AdDU Logo"
+              layout="fill"
+              objectFit="contain"
+              priority
+            />
+          </div>
+        </div>
+        <div className={`${styles["grid-item"]} ${styles["two"]}`}>
+          <div className={styles['video-container']}>
+            <video 
+            src={'/vid1.mp4'}
+            autoPlay
+            loop
+            muted
+            />
+          </div>
+          <h1>SIDLAKAN 22</h1>
+          <div className={styles['h1-hover']}>74th AdDU Fiesta</div>
+        </div>
+        <div className={`${styles["grid-item"]} ${styles["three"]}`}>
+          <div className={styles['video-container']}>
+              <video 
+              src={'/vid.mp4'}
+              autoPlay
+              loop
+              controlsList="nodownload"
+              muted
+              />
+          </div>
+          <NowHappening />
+        </div>
+        <div className={`${styles["grid-item"]} ${styles["four"]}`}>
+          <div className={styles["img_wrapper"]}>
+            <Image
+              src="/cheer.png"
+              alt="Cheer don't jeer"
+              layout="fill"
+              objectFit="cover"
+              priority
+            />
+          </div>
+        </div>
+    
+      </div>
+      <div className={styles['carousel-container']}>
+        <Carousel>
+                <div>
+                    <img src="addu-logo.jpg" />
+                </div>
+                <div>
+                    <img src="assets/2.jpeg" />
+                </div>
+                <div>
+                    <img src="assets/3.jpeg" />
+                </div>
+          </Carousel>
+      </div>
+    </div>
+  )
+}
+
+export default HeroSection
