3e284732d24f1d9780cd12a1006475ac2ade62b2
Innhahn <jaytan3825@gmail.com>
2022-07-30 19:51:57 +0800

    Added Iconify Icons for InfoCard & Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,11 @@
 import styles from '../Footer/Footer.module.scss'
 import Image from "next/image";
+import { Icon } from '@iconify/react';
+import twitterFill from '@iconify/icons-akar-icons/twitter-fill';
+import facebookFill from '@iconify/icons-akar-icons/facebook-fill';
+import instagramFill from '@iconify/icons-akar-icons/instagram-fill';
+import githubFill from '@iconify/icons-akar-icons/github-fill';
+
 
 const Footer = () => {
   return (
@@ -7,7 +13,19 @@ const Footer = () => {
     <div className={styles['section']}>
         <div className={styles['footer-left']}>
           <p> Copyright © 2022 SAMAHAN</p>
-          <p> Developed by <a href = "">SAMAHAN Systems Development </a></p>
+          <p> Developed by SAMAHAN Systems Development </p>
+          <div className={styles['icons-section']}>
+          <a href = "https://twitter.com/SAMAHANSysDev"> <Icon icon={twitterFill} color="white" /></a>
+          </div>
+          <div className={styles['icons-section']}>
+          <a href = "https://www.facebook.com/SAMAHANSysDev"><Icon icon={facebookFill} color="white" /></a>
+          </div>
+          <div className={styles['icons-section']}>
+          <a href = "https://www.instagram.com/samahansysdev/"><Icon icon={instagramFill} color="white" /></a>
+          </div>
+          <div className={styles['icons-section']}>
+          <a href = "https://github.com/SAMAHANSysDev"><Icon icon={githubFill} color="white" /></a>
+          </div>
         </div>
 
         <div className={styles['footer-right']}>
@@ -20,7 +38,6 @@ const Footer = () => {
               objectFit='contain'
               />
           </div>
-
           <div className={styles['img-wrapper']}>
             <Image
               src="/assets/SYSDEV_Logo.png" 
