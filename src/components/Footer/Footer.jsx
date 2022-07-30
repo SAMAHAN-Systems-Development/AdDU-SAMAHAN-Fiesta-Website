4a91ffa6355bb09d5ff4a0fc8d8e0aa5f039c4b8
Innhahn <jaytan3825@gmail.com>
2022-07-30 15:59:48 +0800

    Fixed Footer Error

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,12 +8,9 @@ const Footer = () => {
         <div className={styles['footer-left']}>
         <p> Copyright © 2022 SAMAHAN</p>
         <p> Developed by <a href = "">SAMAHAN Systems Development </a></p>
-     
-
         </div>
 
         <div className={styles['footer-right']}>
-<<<<<<< HEAD
           <h3> SIDLAKAN </h3>
           <div className={styles['img-wrapper']}>
             <Image
@@ -21,7 +18,7 @@ const Footer = () => {
               alt="Smiley Sysdev Logo"
               layout='fill'
               objectFit='contain'
-            />
+              />
           </div>
 
           <div className={styles['img-wrapper']}>
@@ -31,21 +28,11 @@ const Footer = () => {
               layout='fill'
               objectFit='contain'
             />
-=======
           <div>
-            <h3> SIDLAKAN </h3>
-              <div className={styles['img-wrapper']}>
-                <Image
-                  src="/assets/SYSDEV-Logo.png" 
-                  alt="SysDev Logo"
-                  layout='fill'
-                  objectFit='scale-down'
-            
-                />
-              </div>
->>>>>>> 0b78e9308df60038060432728799e60b75dec812
+
           </div>
         </div>
+    </div>
     </div>
   )
 }
