90171e843b05d0bc23023c002cf81c817eae4614
iyabc <juliennepanes@gmail.com>
2022-08-03 00:28:18 +0800

    removed navbar logo anim

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,29 +2,11 @@ import styles from './Navbar.module.scss';
 import { TicketsButton } from '../ComponentIndex';
 import Image from 'next/image';
 import Link from 'next/link';
-import { motion } from "framer-motion"
 
 const Navbar = () => {
-
-  const scaleUp = {
-    initial: {
-      transform: "scale(1)"
-    },
-    animate: {
-      transform: "scale(1.1)"
-    },
-    
-  }
-  
   return (
     <header className={styles.header}>
         <nav className={styles.navbar}>
-          <motion.div
-            className={styles["img_wrapper"]}
-            initial="initial"
-            whileHover="animate"
-            variants={scaleUp}
-          >
             <div className={styles.fiesta_logo}>
               <Link href="/">
                 <Image 
@@ -37,7 +19,6 @@ const Navbar = () => {
                 />
               </Link>
             </div>
-          </motion.div>
             <div className={styles.tickets_btn}>
               <TicketsButton />
             </div>
