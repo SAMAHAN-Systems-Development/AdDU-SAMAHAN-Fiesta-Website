70bf1e1b82bd1106d9bca8b1cc6b508fd4b10ec8
Patrick <patmatayabas@gmail.com>
2022-08-03 14:03:28 +0800

    added mui media query to facebook feed

--- src/components/LandingPage/Socfeeds/FacebookFeed/FacebookFeed.jsx
+++ src/components/LandingPage/Socfeeds/FacebookFeed/FacebookFeed.jsx
@@ -1,11 +1,13 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { FacebookProvider, Page } from 'react-facebook';
 import styles from './FacebookFeed.module.scss'
-
-export default class FacebookFeed extends Component {
-    render() {
+import {useMediaQuery} from '@mui/material';
+export default function FacebookFeed(){
+  
+  const matches = useMediaQuery('(min-width:600px)');
       return (
         <div className={styles["container"]}>
+      
         <h1  className={styles["header"]}> Facebook </h1>
         <div className={styles["container-1"]}>
         <FacebookProvider appId="475123037773076"> 
@@ -14,5 +16,5 @@ export default class FacebookFeed extends Component {
         </div>
         </div>
       );
-    }
+    
 }
--- src/components/LandingPage/SocialsSection/SocialsSection.jsx
+++ src/components/LandingPage/SocialsSection/SocialsSection.jsx
@@ -3,7 +3,9 @@ import styles from '../SocialsSection/SocialsSection.module.scss'
 
 const SocialsSection = () => {
   return (
+   
     <div className={styles['section']}>
+      
       <Socfeeds />
     </div>
   )
