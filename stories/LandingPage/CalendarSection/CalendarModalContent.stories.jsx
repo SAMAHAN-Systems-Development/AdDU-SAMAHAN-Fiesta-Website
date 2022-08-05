2c79239e86a883c04ff254de9612166695999549
tremor6916 <lpawaon@gmail.com>
2022-08-05 11:50:48 +0800

    init storybook for modal and modalcontent

---
+++ stories/LandingPage/CalendarSection/CalendarModalContent.stories.jsx
@@ -0,0 +1,10 @@
+import { CalendarModalContent } from '../../../src/components/ComponentIndex';
+
+export default {
+  title: 'CalendarModalContent',
+  component: CalendarModalContent,
+};
+
+export const Main = {
+  args: {}
+};
