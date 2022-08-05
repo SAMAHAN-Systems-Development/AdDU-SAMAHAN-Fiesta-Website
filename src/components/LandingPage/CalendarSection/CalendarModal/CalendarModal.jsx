3fef3c3d3ec565ac9a213adc1866faf3cfddf486
tremor6916 <lpawaon@gmail.com>
2022-08-05 14:57:37 +0800

    update calendarModal

--- src/components/LandingPage/CalendarSection/CalendarModal/CalendarModal.jsx
+++ src/components/LandingPage/CalendarSection/CalendarModal/CalendarModal.jsx
@@ -1,12 +1,55 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+import Modal from '@mui/material/Modal'
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
 import styles from './CalendarModal.module.scss'
 import CalendarModalContent from './CalendarModalContent/CalendarModalContent.jsx'
 import Calendar from '../../../../data/calendarData.json'
 
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
 const CalendarModal=()=>{
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
     return(
         <div>
-            Goodbye World!
+            {Calendar.events.map((item) =>{
+                return(
+                    <div key={item.title}>
+                        <Button variant="contained" color="primary" onClick={handleOpen}>
+                            {item.title}
+                        </Button>
+                        <Modal open={open} onClose={handleClose}>
+                            <Box style={style}>
+                                <CalendarModalContent title={item.title} sched={item.sched} itemNum={item.itemNum}/>
+                            </Box>
+                        </Modal>
+                    </div>
+                )
+            })}
+        
+           {/*  <Button onClick={handleOpen}>Pls Click</Button>
+            <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+                <Box style={style}>
+                    <h5 id="modal-modal-description">
+                    Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                    </h5>
+                </Box>
+            </Modal>
+        </div>*/}
         </div>
     )
 }
