b9e07d885f7e32faa95e279324ed9f4488b35685
Cyril Olanolan <57930177+CyrilOlanolan@users.noreply.github.com>
2022-08-05 18:14:36 +0800

    Modified styling on CurrentlyHappening, minor changes
        > CurrentlyHappening not centered

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -7,9 +7,6 @@ import SocialsSection from "./SocialsSection/SocialsSection";
 const LandingPage = () => {
   return (
     <div className={styles["LandingPage"]}>
-      {/* FACEBOOK EMBED */}
-      {/* <div id="fb-root"></div>
-      <Script async defer crossOrigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v14.0" nonce="uHPeIijp" /> */}
       <HeroSection />
       <CurrentlyHappening />
       <TribalDivider />
