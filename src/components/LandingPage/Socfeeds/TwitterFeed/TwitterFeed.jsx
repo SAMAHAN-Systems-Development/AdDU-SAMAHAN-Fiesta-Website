3421c9aae92d1e7b6d0fe107b53cea6b5207ed6e
Patrick <patmatayabas@gmail.com>
2022-08-05 15:51:04 +0800

    changed live feed size

--- src/components/LandingPage/Socfeeds/TwitterFeed/TwitterFeed.jsx
+++ src/components/LandingPage/Socfeeds/TwitterFeed/TwitterFeed.jsx
@@ -7,8 +7,8 @@ import {useMediaQuery} from '@mui/material';
 
 export default function TwitterFeed() {
   const matches = useMediaQuery('(min-width:1057px)');
-  const height = matches ? 700 : 700;
-  const width = matches ? 500 : 800;
+  const height = matches ? 700 : 500;
+  const width = matches ? 500 : 300;
   return (
  
     <div className={styles["container"]}>
