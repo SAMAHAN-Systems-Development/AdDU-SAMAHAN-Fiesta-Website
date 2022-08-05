e60eecdd13e788644af442d0937edf4638a52b96
tremor6916 <lpawaon@gmail.com>
2022-08-05 11:50:20 +0800

    init calendarmodal and modalcontent

---
+++ src/components/LandingPage/CalendarSection/CalendarModal/CalendarModal.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './CalendarModal.module.scss'
+import CalendarModalContent from './CalendarModalContent/CalendarModalContent.jsx'
+import Calendar from '../../../../data/calendarData.json'
+
+const CalendarModal=()=>{
+    return(
+        <div>
+            Goodbye World!
+        </div>
+    )
+}
+
+export default CalendarModal
